418852d85798ec9225a094894025f88554e7bda9
Lucas Figueiredo do Monte <127231169+LucasFmonte@users.noreply.github.com>
2024-02-14 09:31:39 -0300

    versao Perto da final

--- index.js
+++ index.js
@@ -1,26 +1,89 @@
-const page1Btn = document.querySelector('#botao1');
-const page2Btn = document.querySelector('#botao2');
-const page3Btn = document.querySelector('#botao3');
-const page1 = document.querySelector('#page1');
-const page2 = document.querySelector('#page2');
-const page3 = document.querySelector('#page3');
+const comecarBtn = document.querySelector('#botaoComecar');
+const pageMotivosBtn = document.querySelector('#botao1');
+const pageSobreBtn = document.querySelector('#botao2');
+const pageExpectativasBtn = document.querySelector('#botao3');
 
-page1Btn.addEventListener('click', () => {
-    page1.style.display = 'flex';
-    page2.style.display = 'none';
-    page3.style.display = 'none';
+const pageInicial = document.querySelector('#pageInicial');
+const pageMotivos = document.querySelector('#pageMotivos');
+const pageSobre = document.querySelector('#pageSobre');
+const pageExpectativas = document.querySelector('#pageExpectativas');
+
+const navMenu = document.querySelector('header');
+
+const fotoTransicao = document.querySelector('#ferramentaTransicao');
+const elementosInvisiveis = document.querySelectorAll('.hidden')
+
+comecarBtn.addEventListener('click', () => {
+    fotoTransicao.classList.toggle('transicionando');
+    intervaloTransicao = setInterval(() => {
+        pageInicial.style.display = 'none';
+        navMenu.style.display = 'flex';
+        pageMotivos.style.display = 'none';
+        pageSobre.style.display = 'flex';
+        pageExpectativas.style.display = 'none';
+        clearInterval(intervaloTransicao);
+    }, 225);
+
+    suporteTransicao = setInterval((() => {
+        fotoTransicao.classList.toggle('transicionando');
+        clearInterval(suporteTransicao);
+    }), 450)
+});
+
+pageMotivosBtn.addEventListener('click', () => {
+    fotoTransicao.classList.toggle('transicionando');
+    intervaloTransicao = setInterval(() => {
+        pageMotivos.style.display = 'flex';
+        pageSobre.style.display = 'none';
+        pageExpectativas.style.display = 'none';
+        clearInterval(intervaloTransicao);
+    }, 250);
+
+    suporteTransicao = setInterval((() => {
+        fotoTransicao.classList.toggle('transicionando');
+        clearInterval(suporteTransicao);
+    }), 500)
+});
+
+pageSobreBtn.addEventListener('click', () => {
+    fotoTransicao.classList.toggle('transicionando');
+    intervaloTransicao = setInterval(() => {
+        pageSobre.style.display = 'flex';
+        pageMotivos.style.display = 'none';
+        pageExpectativas.style.display = 'none';
+        clearInterval(intervaloTransicao);
+    }, 225);
+    
+    suporteTransicao = setInterval((() => {
+        fotoTransicao.classList.toggle('transicionando');
+        clearInterval(suporteTransicao);
+    }), 450)
+});
+
+pageExpectativasBtn.addEventListener('click', () => {
+    fotoTransicao.classList.toggle('transicionando');
+    intervaloTransicao = setInterval(() => {
+        pageExpectativas.style.display = 'flex';
+        pageSobre.style.display = 'none';
+        pageMotivos.style.display = 'none';
+        clearInterval(intervaloTransicao);
+    }, 225);
+    
+    suporteTransicao = setInterval((() => {
+        fotoTransicao.classList.toggle('transicionando');
+        clearInterval(suporteTransicao);
+    }), 450)
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
 })
 
-page2Btn.addEventListener('click', () => {
-    page2.style.display = 'flex';
-    page3.style.display = 'none';
-    page1.style.display = 'none';
-})
-
-page3Btn.addEventListener('click', () => {
-    page3.style.display = 'flex';
-    page2.style.display = 'none';
-    page1.style.display = 'none';
-})
-
-
+elementosInvisiveis.forEach((el) => observer.observe(el));
